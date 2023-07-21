01f6011c3345d48c85fa4d2b7e0702fac01a9702
wilgaslove <wilfried216@gmail.com>
2023-07-21 21:00:46 +0200

    app-music

---
+++ src/app/album.ts
@@ -0,0 +1,20 @@
+export class Album {
+    constructor(
+       public id: string,
+       public ref: string,
+       public name: string,
+       public title: string,
+       public description: string,
+       public duration: number,
+       public status: string,
+       public url?: string,
+       public tags?: Array<string>,
+       public like?: string,
+
+    ) { }
+
+}
+
+export class List {
+
+}
---
+++ src/app/albums/albums.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit} from '@angular/core';
+
+// Importez la définition de la classe et les albums
+import { Album } from '../album';
+import { ALBUMS } from '../mock-albums';
+
+@Component({
+  selector: 'app-albums', // sélecteur à mettre dans le parent
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.css']
+})
+export class AlbumsComponent {
+  titlePage: string = "Page princiaple Albums Music";
+  
+  albums: Album[] = ALBUMS;
+
+  constructor() { }
+  ngOnInit() {
+  }
+}
